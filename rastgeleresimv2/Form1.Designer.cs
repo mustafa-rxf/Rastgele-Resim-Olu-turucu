51fb8c6e4e851707ee5701dddac9ea55ee64a0ca
Mustafa Uzuner <dehsethurma@gmail.com>
2026-02-02 11:59:25 +0300

    Rastgele Resim Çekme Ve Sunum Modu Eklendi, Program Geliştiriliyor.

--- rastgeleresimv2/Form1.Designer.cs
+++ rastgeleresimv2/Form1.Designer.cs
@@ -2,38 +2,108 @@
 {
     partial class Form1
     {
-        /// <summary>
-        /// Required designer variable.
-        /// </summary>
         private System.ComponentModel.IContainer components = null;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button btnYeniResim;
+        private System.Windows.Forms.Button btnSunumModu;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Label lblLoading;
+        private System.Windows.Forms.Timer slideTimer;
+        private System.Windows.Forms.Panel fadePanel;
+        private System.Windows.Forms.Timer fadeTimer;
 
-        /// <summary>
-        /// Clean up any resources being used.
-        /// </summary>
-        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
         protected override void Dispose(bool disposing)
         {
             if (disposing && (components != null))
-            {
                 components.Dispose();
-            }
             base.Dispose(disposing);
         }
 
         #region Windows Form Designer generated code
 
-        /// <summary>
-        /// Required method for Designer support - do not modify
-        /// the contents of this method with the code editor.
-        /// </summary>
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.btnYeniResim = new System.Windows.Forms.Button();
+            this.btnSunumModu = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.lblLoading = new System.Windows.Forms.Label();
+            this.slideTimer = new System.Windows.Forms.Timer(this.components);
+            this.fadePanel = new System.Windows.Forms.Panel();
+            this.fadeTimer = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Size = new System.Drawing.Size(600, 400);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            // 
+            // btnYeniResim
+            // 
+            this.btnYeniResim.Location = new System.Drawing.Point(12, 420);
+            this.btnYeniResim.Size = new System.Drawing.Size(295, 35);
+            this.btnYeniResim.Text = "Yeni Resim";
+            this.btnYeniResim.Click += new System.EventHandler(this.btnYeniResim_Click);
+            // 
+            // btnSunumModu
+            // 
+            this.btnSunumModu.Location = new System.Drawing.Point(317, 420);
+            this.btnSunumModu.Size = new System.Drawing.Size(295, 35);
+            this.btnSunumModu.Text = "Sunum Modu";
+            this.btnSunumModu.Click += new System.EventHandler(this.btnSunumModu_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(162, 190);
+            this.progressBar1.Size = new System.Drawing.Size(300, 12);
+            this.progressBar1.Style = System.Windows.Forms.ProgressBarStyle.Marquee;
+            this.progressBar1.Visible = false;
+            // 
+            // lblLoading
+            // 
+            this.lblLoading.AutoSize = true;
+            this.lblLoading.Location = new System.Drawing.Point(245, 170);
+            this.lblLoading.Text = "Fotoğraf yükleniyor...";
+            this.lblLoading.Visible = false;
+            // 
+            // fadePanel
+            // 
+            this.fadePanel.BackColor = System.Drawing.Color.Black;
+            this.fadePanel.Visible = false;
+            this.fadePanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            // 
+            // slideTimer
+            // 
+            this.slideTimer.Interval = 6000;
+            this.slideTimer.Tick += new System.EventHandler(this.slideTimer_Tick);
+            // 
+            // fadeTimer
+            // 
+            this.fadeTimer.Interval = 30;
+            this.fadeTimer.Tick += new System.EventHandler(this.fadeTimer_Tick);
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(624, 470);
+            this.Controls.Add(this.fadePanel);
+            this.Controls.Add(this.btnSunumModu);
+            this.Controls.Add(this.btnYeniResim);
+            this.Controls.Add(this.lblLoading);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.pictureBox1);
+            this.KeyPreview = true;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.Text = "Rastgele Resim";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
         }
 
         #endregion
     }
 }
-
--- rastgeleresimv2/Form1.cs
+++ rastgeleresimv2/Form1.cs
@@ -1,20 +1,110 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Net;
 using System.Windows.Forms;
 
 namespace rastgeleresimv2
 {
     public partial class Form1 : Form
     {
+        private bool sunumModu = false;
+        private int fadeAlpha = 0;
+        private bool fadingIn = true;
+
         public Form1()
         {
+            ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            RastgeleResimYukle(false);
+        }
+
+        private void btnYeniResim_Click(object sender, EventArgs e)
+        {
+            RastgeleResimYukle(true);
+        }
+
+        private void RastgeleResimYukle(bool normalMod)
+        {
+            if (normalMod && !sunumModu)
+            {
+                progressBar1.Visible = true;
+                lblLoading.Visible = true;
+            }
+
+            string url = "https://picsum.photos/1920/1080?random=" + Guid.NewGuid();
+            pictureBox1.LoadAsync(url);
+
+            if (sunumModu)
+                fadeTimer.Start();
+        }
+
+        private void btnSunumModu_Click(object sender, EventArgs e)
+        {
+            sunumModu = true;
+
+            FormBorderStyle = FormBorderStyle.None;
+            WindowState = FormWindowState.Maximized;
+            pictureBox1.Dock = DockStyle.Fill;
+
+            btnYeniResim.Visible = false;
+            btnSunumModu.Visible = false;
+            progressBar1.Visible = false;
+            lblLoading.Visible = false;
+
+            slideTimer.Start();
+        }
+
+        private void slideTimer_Tick(object sender, EventArgs e)
+        {
+            fadingIn = true;
+            fadeAlpha = 0;
+            fadePanel.Visible = true;
+            fadeTimer.Start();
+        }
+
+        private void fadeTimer_Tick(object sender, EventArgs e)
+        {
+            if (fadingIn)
+            {
+                fadeAlpha += 20;
+                fadePanel.BackColor = System.Drawing.Color.FromArgb(fadeAlpha, 0, 0, 0);
+                if (fadeAlpha >= 200)
+                {
+                    fadingIn = false;
+                    RastgeleResimYukle(false);
+                }
+            }
+            else
+            {
+                fadeAlpha -= 20;
+                fadePanel.BackColor = System.Drawing.Color.FromArgb(fadeAlpha, 0, 0, 0);
+                if (fadeAlpha <= 0)
+                {
+                    fadeTimer.Stop();
+                    fadePanel.Visible = false;
+                }
+            }
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Escape && sunumModu)
+            {
+                slideTimer.Stop();
+                fadeTimer.Stop();
+                sunumModu = false;
+
+                FormBorderStyle = FormBorderStyle.FixedSingle;
+                WindowState = FormWindowState.Normal;
+                pictureBox1.Dock = DockStyle.None;
+                pictureBox1.SetBounds(12, 12, 600, 400);
+
+                btnYeniResim.Visible = true;
+                btnSunumModu.Visible = true;
+            }
         }
     }
 }
